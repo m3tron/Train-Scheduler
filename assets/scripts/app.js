e444961fd9788bd5b5a4dff4665f726e6ddf8e55
Sunny Heyar <sunny.heyar@gmail.com>
2019-03-15 17:10:27 -0400

    uploaded html

---
+++ assets/scripts/app.js
@@ -0,0 +1,11 @@
+var config = {
+  apiKey: "AIzaSyAglDkkOG2I7XI03NOEMS8BF4vfwItSTRU",
+  authDomain: "train-scheduler-27d9f.firebaseapp.com",
+  databaseURL: "https://train-scheduler-27d9f.firebaseio.com",
+  projectId: "train-scheduler-27d9f",
+  storageBucket: "train-scheduler-27d9f.appspot.com",
+  messagingSenderId: "931723817378"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
