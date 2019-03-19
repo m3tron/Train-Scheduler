58cd0ade1433b4ae81b99552f877297e7987bbe2
Sunny Heyar <sunny.heyar@gmail.com>
2019-03-19 01:11:26 -0400

    js working

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -14,35 +14,58 @@ $(document).ready(function() {
   database.ref().on("child_added", function(snapshot, prevKey) {
     var data = snapshot.val();
 
-    var tr = $("<tr>");
-    tr.append($("<th class='font-weight-normal'>").text(data.trainName));
-    tr.append($("<th class='font-weight-normal'>").text(data.trainDestination));
-    tr.append($("<th class='font-weight-normal'>").text(data.trainFrequency));
-    tr.append($("<th class='font-weight-normal'>").text(data.trainTime));
+    //minutes remaining to next train
+    var minutesAway =
+      data.trainFrequency -
+      (moment().diff(moment.unix(data.firstTrainTime), "minutes") %
+        data.trainFrequency);
 
+    //arrival time of next train
+    var nextArrival = moment()
+      .add(minutesAway, "m")
+      .format("hh:mm A");
+
+    //displays information from database to train schedule
+    var tr = $("<tr>");
+    tr.append(
+      $("<th class='font-weight-normal text-capitalize'>").text(data.trainName)
+    );
+    tr.append(
+      $("<th class='font-weight-normal text-capitalize'>").text(
+        data.trainDestination
+      )
+    );
+    tr.append($("<th class='font-weight-normal'>").text(data.trainFrequency));
+    tr.append($("<th class='font-weight-normal'>").text(nextArrival));
+    tr.append($("<th class='font-weight-normal'>").text(minutesAway));
     $("tbody").append(tr);
   });
 
   $("#submit").on("click", function(e) {
     e.preventDefault();
+
+    //stores data from input into variables
     var name = $("#trainName")
       .val()
       .trim();
     var destination = $("#trainDestination")
       .val()
       .trim();
-    var time = $("#trainTime")
-      .val()
-      .trim();
+    var time = moment(
+      $("#trainTime")
+        .val()
+        .trim(),
+      "HH:mm"
+    ).format("X");
     var frequency = $("#trainFrequency")
       .val()
       .trim();
-    console.log(name, destination, time, frequency);
 
+    //pushes data to database
     database.ref().push({
       trainName: name,
       trainDestination: destination,
-      trainTime: time,
+      firstTrainTime: time,
       trainFrequency: frequency
     });
   });
